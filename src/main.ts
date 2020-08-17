80b229bfd0b2a362fe06d0062257cee757848cdb
liuxiang <1753439422@qq.com>
2020-08-17 14:26:39 +0800

    feat: 添加错误处理

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // 全局使用中间件
   // app.use(logger)
+
+  // 全局过滤器
+  // app.useGlobalFilters(new HttpExceptionFilter());
 
   // 设置swagger文档相关配置
   const swaggerOptions = new DocumentBuilder()
---
+++ src/modules/exception/exception.controller.ts
@@ -0,0 +1,59 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Query,
+  Delete,
+  Body,
+  Param,
+  Headers,
+  UseFilters,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { ApiTags, ApiBearerAuth, ApiBody, ApiParam, ApiQuery } from '@nestjs/swagger';
+import { ExceptionService } from './exception.service';
+import { HttpExceptionFilter } from '../../common/filters/http-exception.filter';
+
+@ApiBearerAuth()
+@ApiTags('exception')
+@UseFilters(new HttpExceptionFilter())
+@Controller('exception')
+export class ExceptionController {
+  constructor(private readonly exceptionService: ExceptionService) {
+  }
+
+  // 查询
+  @Get()
+  fetch(@Query() { id }, @Headers('token') token): string {
+    console.log(token);
+    if (!id) {
+      throw new HttpException({
+        status: HttpStatus.BAD_REQUEST, message: '请求参数id 必传', error: 'id is required',
+      }, HttpStatus.BAD_REQUEST);
+    }
+    return this.exceptionService.fetch(id);
+  }
+
+  @Post()
+  @ApiBody({ description: '请输入message' })
+  save(@Body() { message }): string {
+    return this.exceptionService.save(message);
+  }
+
+  @Patch(':id')
+  @ApiParam({ name: 'id' })
+  @ApiBody({ description: '请输入更新内容' })
+  update(@Param() { id }, @Body() { message }): string {
+    return this.exceptionService.update(id, message);
+  }
+
+  @Delete()
+  @ApiQuery({ name: 'id' })
+  remove(@Query() { id }): string {
+    return this.exceptionService.remove(id);
+  }
+
+
+}
