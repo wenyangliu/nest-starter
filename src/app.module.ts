253461dcfd4be1579395b484c3e3aa312313789f
liuxiang <1753439422@qq.com>
2020-08-18 10:12:04 +0800

    feat: 邮件发送

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,36 @@
+import * as path from 'path';
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { HelloModule } from './modules/hello/hello.module';
 import { ExceptionModule } from './modules/exception/exception.module';
 import { RoleGuardModule } from './modules/role-guard/role-guard.module';
+import { EmailModule } from './modules/email/email.module';
 
 @Module({
-  imports: [HelloModule, ExceptionModule, RoleGuardModule],
+  imports: [
+    MailerModule.forRootAsync({
+      // *** 代表16位授权码 替换
+      useFactory: () => ({
+        transport: 'smtps://1753439422@qq.com:***@smtp.qq.com',
+        defaults: {
+          from: '"nest-modules" <modules@nestjs.com>',
+        },
+        template: {
+          dir: path.join(__dirname, './templates/email'),
+          adapter: new PugAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+    }),
+    HelloModule,
+    ExceptionModule,
+    RoleGuardModule,
+    EmailModule,
+  ],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
---
+++ src/modules/email/email.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
+
+@Injectable()
+export class EmailService {
+  constructor(private readonly mailerService: MailerService) {}
+
+  sendEmail() {
+    this.mailerService.sendMail({
+      // to: '18860469266@163.com',
+      to: '1296896549@qq.com',
+      from: '1753439422@qq.com',
+      subject: '1501',
+      html: '<b>猜猜我是谁!</b>'
+      // template: 'welcome'
+    })
+  }
+}
